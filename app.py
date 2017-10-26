ebd81f04af3a420b477f07c2939309055fc6509b
Daniel Levy <dlevy4585@gmail.com>
2017-10-26 13:10:13 +0300

    commented

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def webhook():
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
-    if data["object"] == "page":
+    #if data["object"] == "page":
 
         #for entry in data["entry"]:
         #    for messaging_event in entry["messaging"]:
