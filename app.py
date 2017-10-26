2bb304dc0b7bbc7c1d4af41c51172cd76687d94c
Daniel Levy <dlevy4585@gmail.com>
2017-10-26 14:24:59 +0300

    read messages

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def webhook():
                         sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                         recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                         message_text = messaging_event["message"]["text"]  # the message's text
-                        send_message(sender_id, "בר רפאלי")
+                        send_message(sender_id, "בר רפאלי".encode("UTF-8"))
 
                     if messaging_event.get("delivery"):  # delivery confirmation
                         pass
@@ -61,7 +61,7 @@ def send_message(recipient_id, message_text):
             "id": recipient_id
         },
         "message": {
-            "text": message_text.encode("UTF-8")
+            "text": message_text
         }
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
