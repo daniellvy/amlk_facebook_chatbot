112d413d97f4f7e8b3b66cfb4ec53b37b31e0938
Daniel Levy <dlevy4585@gmail.com>
2017-10-26 13:07:47 +0300

    commented

--- app.py
+++ app.py
@@ -31,25 +31,25 @@ def webhook():
 
     if data["object"] == "page":
 
-        for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
+        #for entry in data["entry"]:
+        #    for messaging_event in entry["messaging"]:
 
-                if messaging_event.get("message"):  # someone sent us a message
+        #        if messaging_event.get("message"):  # someone sent us a message
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+        #            sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+        #            recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+        #            message_text = messaging_event["message"]["text"]  # the message's text
 
-                    send_message(sender_id, "roger that!")
+        #            send_message(sender_id, "roger that!")
 
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
+        #        if messaging_event.get("delivery"):  # delivery confirmation
+        #            pass
 
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
+        #        if messaging_event.get("optin"):  # optin confirmation
+        #            pass
 
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
+        #        if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+        #            pass
 
     return "ok", 200
 
