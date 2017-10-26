1b6cac75b0aab4e9889611e3c16403239bcba2a9
Daniel Levy <dlevy4585@gmail.com>
2017-10-26 14:26:06 +0300

    Answer to all messages is Bar Refaeli

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def webhook():
                         sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                         recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                         message_text = messaging_event["message"]["text"]  # the message's text
-                        send_message(sender_id, "בר רפאלי".encode("UTF-8"))
+                        send_message(sender_id, "Bar Refaeli")
 
                     if messaging_event.get("delivery"):  # delivery confirmation
                         pass
